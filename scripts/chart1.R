4d81d60b265def56841f5f057eee4cbe7800003a
yuhanizaly <yaly@uw.edu>
2020-11-29 13:23:16 -0800

    revised chart1 R file and index file

--- scripts/chart1.R
+++ scripts/chart1.R
@@ -1,3 +1,4 @@
+# Chart 1 - line graph
 # load packages
 library("dplyr")
 library("tidyr")
@@ -10,16 +11,16 @@ library("stringr")
 # load data
 data <- read.csv("data/disney_movies.csv", stringsAsFactors = FALSE)
 
-# Describe the pattern of the gross (total gross and infaltion adjusted gross) 
+# Describe the pattern of the gross (total gross and infaltion adjusted gross)
 # over time (years)
   info_disney <- data %>%
     mutate(year = format(as.Date(release_date), "%Y")) %>%
-    group_by(year) %>% 
+    group_by(year) %>%
     summarise(
       total_gross = sum(total_gross, na.rm = TRUE),
       inflation_adj_gross = sum(inflation_adjusted_gross, na.rm = TRUE)) %>%
     gather("key", "value", total_gross, inflation_adj_gross) %>%
-    mutate(label = str_to_title(gsub( "_", " ", key))) 
+    mutate(label = str_to_title(gsub("_", " ", key)))
 
 # Create the line graph on ggplot
   line_graph_disney <- ggplot(data = info_disney) +
@@ -35,38 +36,7 @@ data <- read.csv("data/disney_movies.csv", stringsAsFactors = FALSE)
       size = .5) +
     labs(title = "The Yearly Gross of Disney Movies Over Time",
       x = "Year",
-      y = "Value (USD)" 
+      y = "Value (USD)"
   )
 
 plot_line_graph <- ggplotly(line_graph_disney, tooltip = "text")
-
-# Two Important Inforation 
-# Highest inflation adjusted gross
-highest_inf_adj_gross <- data %>%
-  mutate(year = format(as.Date(release_date), "%Y")) %>%
-  group_by(year) %>%
-  summarise(highest_inflation = sum(inflation_adjusted_gross, na.rm = TRUE)) %>%
-  filter(highest_inflation == max(highest_inflation)) %>%
-  pull(year)
-
-# Lowest inflation adjusted gross
-lowest_inf_adj_gross <- data %>%
-  mutate(year = format(as.Date(release_date), "%Y")) %>%
-  group_by(year) %>%
-  summarise(lowest_inflation = sum(inflation_adjusted_gross, na.rm = TRUE)) %>%
-  filter(lowest_inflation == min(lowest_inflation)) %>%
-  pull(year)
-
-# Top 10 years with the highest adjusted inflation
-top_10_years <- data %>%
-  mutate(year = format(as.Date(release_date), "%Y")) %>%
-  group_by(year) %>%
-  summarise(highest_inflation = sum(inflation_adjusted_gross, na.rm = TRUE)) %>%
-  arrange(desc(highest_inflation)) %>%
-  head(10) %>%
-  pull(year)
-
-
-# Question: How is the trend of the yearly gross of Disney movies changing over time between between 1937-2016? 
-Over time, 
-  

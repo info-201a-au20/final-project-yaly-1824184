819f57e6755041da2f9070b1247dee4b22c99abe
Siya Sharma <siyasharma@siyas-mbp.lan>
2020-11-29 20:11:32 -0800

    fixed function for summary

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -1,29 +1,7 @@
-rm(list = ls())
-
-library(dplyr)
+library("dplyr")
 
 # data set
 disney_movies_df <- read.csv("data/disney_movies.CSV", stringsAsFactors = FALSE)
-
-Highest_Gross_Movie <- disney_movies_df %>% 
-  filter(total_gross == max(total_gross, na.rm = TRUE)) %>%
-  pull(movie_title)
-
-Lowest_Gross_Movies <- disney_movies_df %>% 
-  filter(total_gross == min(total_gross, na.rm = TRUE)) %>%
-  pull(movie_title)
-
-Most_Common_Genre <- disney_movies_df %>%
-  filter(genre == max(genre, na.rm = TRUE)) %>%
-  head(n = 1) %>%
-  pull(genre)
-
- Most_Common_Release_Year <- disney_movies_df %>%
-  mutate("years" = substring(release_date, 1, 4)) %>%
-  filter(release_date == max(release_date, na.rm = TRUE)) %>%
-  pull(years)
-
-Total_Movies <- nrow(disney_movies_df)
 
 summary_info <- function(df) {
   Info <- list(Highest_Gross_Movie = df %>% 
@@ -41,8 +19,7 @@ summary_info <- function(df) {
                  filter(release_date == max(release_date, na.rm = TRUE)) %>%
                  pull(years),
                Total_Movies = nrow(df))
-  return (Info)
+  return(Info)
 } 
-
 summary <- summary_info(disney_movies_df)
 
--- scripts/summary_table.R
+++ scripts/summary_table.R
@@ -1,8 +1,5 @@
-rm(list = ls())
-
-library(dplyr)
-
-library(knitr)
+library("dplyr")
+library("knitr")
 
 disney_movies_df <- read.csv("data/disney_movies.CSV", stringsAsFactors = FALSE)
 

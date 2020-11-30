ca670774afefbb9fe1160dc4c5c6c4eb3ecd698a
[Cole Miller] <[colem15@uw.edu]>
2020-11-29 20:16:19 -0800

    fixed table

--- scripts/summary_table.R
+++ scripts/summary_table.R
@@ -1,18 +1,18 @@
-rm(list = ls())
-
 library(dplyr)
 
-library(knitr)
 
-disney_movies_df <- read.csv("data/disney_movies.CSV", stringsAsFactors = FALSE)
+#Takes a dataset and reteurns a summary table
+summary_table <- function(dataset){
+  table <- group_by(dataset, genre) %>%
+    na_if("") %>%
+    filter(!is.na(genre)) %>% 
+    summarize(number = n()) %>%
+    arrange(-number) %>%
+    rename("Number of Movies" = number)
+    
+  
+  return(table)
+}
 
-summary_table <- disney_movies_df %>%
-  group_by(movie_title) %>%
-  filter (inflation_adjusted_gross == mean(inflation_adjusted_gross)) %>%
-  summarize(Genre = genre,
-            Mpaa = mpaa_rating,
-            inflation_gross = mean (inflation_adjusted_gross)) %>%
-            head(n = 10)
 
-table <- kable(summary_table)
   

fcbc5ff75a61b72b96eb34987c2d712e65f48904
Siya Sharma <siyasharma@siyas-mbp.lan>
2020-12-06 16:47:28 -0800

    added ui and server stuff for 4th page

--- app_server.R
+++ app_server.R
@@ -2,6 +2,20 @@ library("shiny")
 library("ggplot2")
 library("dplyr")
 
+# Final Project
 server <- function(input, output) {
-  
+  # creates bar chart
+  output$bar <- renderPlot({
+    title <- "Genre V.S. Count of the Highest Grossing Disney Films"
+    
+    if (input$which_set == 1) {
+      p <- ggplot(top_10) 
+    } else {
+      p <- ggplot(top_20) 
+    }
+    p <- p + geom_bar(mapping = aes_string(x = "genre", fill = input$bar_fill), 
+                      position = "stack") +
+      labs(title = title, x = "Genre", fill = input$bar_fill, y = "Count")
+    p
+  })
 }
--- app_ui.R
+++ app_ui.R
@@ -3,6 +3,8 @@ library("ggplot2")
 library("dplyr")
 library("tidylr")
 
+movies <- read.csv("data/disney_movies.csv", stringsAsFactors = FALSE)
+
 #MULTIPLE TABS FORMAT
 # Intro page
 page_one <- tabPabel(
@@ -37,14 +39,49 @@ page_three <- tabPanel(
 )
 
 # 3rd chart
+# dataset of the top 20 highest grossing films
+top_20 <- movies %>%
+  filter(mpaa_rating != "") %>%
+  top_n(20, inflation_adjusted_gross) %>%
+  mutate(year = substring(release_date, 1, 4))
+
+# dataset of the top 10 highest gorssing disney films
+top_10 <- movies %>%
+  filter(mpaa_rating != "") %>%
+  top_n(10, inflation_adjusted_gross) %>%
+  mutate(year = substring(release_date, 1, 4))
+
+# allows user to choose which column they want to use for fill in bar chart
+bar_fill <- selectInput(
+  "bar_fill",
+  label = "Fill for Bar Chart",
+  choices = list("mpaa_rating", "year"),
+  selected = "mpaa_rating"
+)
+
+# allows user to choose to compare the top 10 or 20 highest grossing films
+which_movies <- radioButtons(
+  "which_set",
+  label = "Top 10 or Top 20 Films?",
+  choices = list("Top 10" = 1, "Top 20" = 2),
+  selected = 2
+)
+
 page_four <- tabPanel(
-  "Page four",
+  "Top Grossing Films",
   sidebarLayout(
     sidebarPanel(
-      
+      bar_fill,
+      which_movies
     ),
     mainPanel(
-      
+      h1("Comparing the Highest-Grossing Disney Films"),
+      p("This chart compares the highest grossing Disney films of all time. On
+        the left hand side, choose if you want to compare the top 10 or 20 
+        movies than see the genre and ratings of these films. We can study which
+        types of movies are most succesful. You can also study which years these
+        top movies were made."),
+      plotOutput("bar")
     )
   )
 )

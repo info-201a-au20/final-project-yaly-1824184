326f48e4f00b2436c5cf5ae921bd95e27c0912cb
Farias29 <Farias29@uw.edu>
2020-12-06 20:08:55 -0800

    added summary

--- app_ui.R
+++ app_ui.R
@@ -5,10 +5,67 @@ library("tidylr")
 
 #MULTIPLE TABS FORMAT
 # Intro page
-page_one <- tabPabel(
-  "Page one",
-
+page_one <- tabPanel (
+  tags$body( "Summary",
+             
+             mainPanel(
+               h1("Disney Movies"),
+               
+               img(src="Walt-Disney-Pictures-Logo.jpg", width = "1050px", 
+                   height = "521px"),
+               
+               p("We are interested in exploring the domain of entertainment 
+               because it is a very broad field with lots of available data in 
+               part due to the rise of streaming services. There are many 
+               different ways people use their free time on entrainment today.
+               Such as TV, Netflix, Spotify, YouTube, Disney +, Movies, 
+               Podcasts, Games, Twitter, etc. We have all been consuming a lot
+               of entertainment recently because of COVID-19.")
+               ,
+               
+               h2("Summary"),
+               
+               p("We are interested in exploring the domain of entertainment 
+               because it is a very broad field with lots of available data in 
+               part due to the rise of streaming services. There are many 
+               different ways people use their free time on entrainment today. 
+               Such as TV, Netflix, Spotify, YouTube, Disney +, Movies, 
+               Podcasts, Games, Twitter, etc. We have all been consuming a lot 
+               of entertainment recently because of COVID-19."),
+               
+               h2("Major Questions"),
+               
+               p("These are questions we seek to answer:"),
+               
+               tags$ol(
+                 
+                 tags$li("What characteristics of a video make it go viral? 
+                 Analysis of viral videos to compare length, titles, title 
+                 length, genre, language, etc."),
+                 
+                 
+                 tags$li("How does a type of entertainment keep audiences 
+                 engaged? Changing seasons, new cast, how often, adding 
+                 challenges?"),
+                 
+                 
+                 tags$li("What type of entertainment is most popular from over 
+                 the years? Comparison of money involved or amount of time 
+                 people spend on a certain type etc.")
+               ),
+               
+               
+               h2("Data used to Answer"),
+               
+               p("We will be using the tags$em", tags$em("disney_movies"), "data
+               set to answer our major questions about", tags$b("Disney"), 
+               "movies. This data set includes all", tags$b("Disney"), "movies 
+               up to 2016. It has  their release dates, genre, rating, total 
+               gross and inflation adjusted gross(2016).")
+             )
+      )
 )
+
 
 # 1st chart
 page_two <- tabPanel(
